3c6384a360bd70f4130fa50e8056f6529b85a637
SushiTee <weichel.sascha@xxx.xx>
2011-02-11 23:21:48 +0100

    - fixed taking flag - fixed compiling error in client

--- src/game/client/components/particles.cpp
+++ src/game/client/components/particles.cpp
@@ -2,6 +2,7 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <base/math.h>
 #include <engine/graphics.h>
+#include <engine/demo.h>
 
 #include <game/generated/client_data.h>
 #include <game/client/render.h>
