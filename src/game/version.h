8e22452e804597f4b9507f8158983235040b6d7a
oy <Tom_Adams@web.de>
2011-02-16 11:32:40 +0100

    changed netversion to 0.6

--- src/game/version.h
+++ src/game/version.h
@@ -3,6 +3,6 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.c"
-#define GAME_VERSION "0.5.2" // fake version here
-#define GAME_NETVERSION "0.5 b67d1f1a1eea234e"
+#define GAME_VERSION "0.6 trunk"
+#define GAME_NETVERSION "0.6 " GAME_NETVERSION_HASH
 #endif
