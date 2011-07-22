228f79dfbea9c928d367f1d75e9d42ef92c7c2ba
Johannes Loher <ghost91@gmx.net>
2011-07-22 23:17:16 +0200

    Added server sided automatic demo recording

--- src/engine/server.h
+++ src/engine/server.h
@@ -58,6 +58,8 @@ public:
 	
 	virtual bool IsAuthed(int ClientID) = 0;
 	virtual void Kick(int ClientID, const char *pReason) = 0;
+
+	virtual void DemoRecorder_HandleAutoStart() = 0;
 };
 
 class IGameServer : public IInterface
